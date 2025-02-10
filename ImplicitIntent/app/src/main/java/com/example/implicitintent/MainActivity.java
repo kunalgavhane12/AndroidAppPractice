838418be9d751dbd471eeac05a604067fda2bda1
kunalgavhane12 <kunalgavhane12@gmail.com>
2025-02-10 15:25:43 +0530

    Implicit Intent

---
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.implicitintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button btnDial, btnMsg, btnEmail, btnShare;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnDial =findViewById(R.id.btnDial);
+        btnMsg =findViewById(R.id.btnMsg);
+        btnShare =findViewById(R.id.btnShare);
+        btnEmail =findViewById(R.id.btnEmail);
+
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iDial = new Intent(Intent.ACTION_DIAL);
+//                iDial.setAction(Intent.ACTION_DIAL);
+                iDial.setData(Uri.parse("tel: +919922257744"));
+                startActivity(iDial);
+            }
+        });
+
+        btnMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iMsg = new Intent(Intent.ACTION_SENDTO);
+                iMsg.setData(Uri.parse("smsto:"+Uri.encode("+919922257744")));
+                iMsg.putExtra("sms_body", "Hello");
+                startActivity(iMsg);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iEmail = new Intent(Intent.ACTION_SEND);
+                iEmail.setType("message/rfc822");
+                iEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"kunalgavhane12@gmail.com", "kunal.gavhane@deltaiot.in"});
+                iEmail.putExtra(Intent.EXTRA_SUBJECT, "Queries");
+                iEmail.putExtra(Intent.EXTRA_TEXT, "Please Resolve this issue");
+                startActivity(Intent.createChooser(iEmail, "Email via"));
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iShare = new Intent(Intent.ACTION_SEND);
+                iShare.setType("text/plain");
+                iShare.putExtra(Intent.EXTRA_TEXT, "Download this Amazing App, https://play.google.com/store/apps/details?id=com.instagram.android&hl=en_US");
+                startActivity(Intent.createChooser(iShare, "Share via"));
+            }
+        });
+    }
+}
