347494e2d93b7ed3d5ade7c8e4cbde20df489408
kunalgavhane12 <kunalgavhane12@gmail.com>
2025-03-04 18:47:20 +0530

    Camera and gallery

---
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.cameraapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgCamera;
+    private final int CAMERA_REQ_CODE = 100;
+    private final int GALLERY_REQ_CODE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imgCamera = findViewById(R.id.imgCamera);
+        Button btnCamera = findViewById(R.id.btnCamera);
+        Button btnGallery = findViewById(R.id.btngallery);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(iCamera, CAMERA_REQ_CODE );
+            }
+        });
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQ_CODE) {
+                // for camera
+                Bitmap img = (Bitmap) data.getExtras().get("data");
+                imgCamera.setImageBitmap(img);
+            } else if (requestCode == GALLERY_REQ_CODE) {
+                //for gallery
+                imgCamera.setImageURI(data.getData());
+            }
+        }
+    }
+}
+
+
